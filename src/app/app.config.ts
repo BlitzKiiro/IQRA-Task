f0ae0c21a76a52fa4e0cf594bc1a40ac75fe4dbe
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-12-01 08:58:14 +0200

    Angular Fire Setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { importProvidersFrom } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment.development';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(AngularFireModule.initializeApp(environment.firebase)),
+  ],
 };
