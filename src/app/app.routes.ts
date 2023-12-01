96d301c09a3e02cdf90d46ea3eafb4c84faca294
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-12-01 14:31:25 +0200

    Nested Layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { Page404Component } from './page404/page404.component';
+import { RootComponent } from './root/root.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: '',
+    component: RootComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'register', component: RegisterComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+    ],
+  },
+  { path: '**', component: Page404Component },
 ];
