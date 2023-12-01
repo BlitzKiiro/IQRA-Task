36ad27a35921dd20afbcfb06414126358f5f52a4
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-12-01 08:14:24 +0200

    env & dependencies setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBzqRcfhvucaOMw_QXk6VA_ZTPXhf-G3pg',
+    authDomain: 'iqra-task.firebaseapp.com',
+    projectId: 'iqra-task',
+    storageBucket: 'iqra-task.appspot.com',
+    messagingSenderId: '790595223170',
+    appId: '1:790595223170:web:e8baff162b2897229d3e9b',
+    measurementId: 'G-SWD5G5FHYF',
+  },
+};
