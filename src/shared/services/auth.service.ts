175252c154c2480485e469fe0cf09557b8026cbc
Ahmed Azmy <ahmedazmycs@protonmail.com>
2023-12-01 10:49:22 +0200

    Auth Service

---
+++ src/shared/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  current_user: firebase.User | null = null;
+
+  constructor(private afAuth: AngularFireAuth) {
+    this.current_user = JSON.parse(localStorage.getItem('user') || 'null');
+
+    this.afAuth.authState.subscribe((user) => {
+      this.current_user = user;
+      localStorage.setItem('user', JSON.stringify(user));
+    });
+  }
+
+  async loginWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    const credentials = await this.afAuth.signInWithEmailAndPassword(
+      email,
+      password
+    );
+    return credentials;
+  }
+
+  async logout(): Promise<void> {
+    await this.afAuth.signOut();
+    this.current_user = null;
+  }
+}
